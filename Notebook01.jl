ea4bbc2249f12ace9e38671dd8376e9b798cb4e1
Ankur Satyarthi <ankurindiaofficial@gmail.com>
2023-01-04 22:24:56 +0530

    made some changes #0001

--- Notebook01.jl
+++ Notebook01.jl
@@ -5,3 +5,13 @@ first(oldcarprice,5)
 last(oldcarprice,5)
 describe(oldcarprice)
 # vscodedisplay(oldcarprice)
+x=oldcarprice.kms_driven
+y=oldcarprice.car_prices_in_rupee
+plot(x,y)
+
+
+
+
+using BenchmarkTools
+@btime $oldcarprice[1:end-1,1:end-1];
+@btime @view $oldcarprice[1:end-1,1:end-1]
