ed2456b81f376c910ab706db84c1a0dc411633d0
Ankur Satyarthi <ankurindiaofficial@gmail.com>
2024-03-05 09:13:03 +0530

    Made few changes

--- Notebook01.jl
+++ Notebook01.jl
@@ -1,17 +1,12 @@
-using CSV, DataFrames, TypedTables, GLM, Plots, Makie
-oldcarprice=CSV.File("data/car_price.csv")
-# oldcarprices=DataFrame(CSV.File("data/car_price.csv"))
-first(oldcarprice,5)
-last(oldcarprice,5)
-describe(oldcarprice)
+using CSV, DataFrames, TypedTables, GLM, Plots, Makie, BenchmarkTools
+oldcarprices=DataFrame(CSV.File("data/car_price.csv"))
+first(oldcarprices,5)
+last(oldcarprices,5)
+describe(oldcarprices)
 # vscodedisplay(oldcarprice)
-x=oldcarprice.kms_driven
-y=oldcarprice.car_prices_in_rupee
-plot(x,y)
+x=oldcarprices.kms_driven
+y=oldcarprices.car_prices_in_rupee
 
 
-
-
-using BenchmarkTools
 @btime $oldcarprice[1:end-1,1:end-1];
 @btime @view $oldcarprice[1:end-1,1:end-1]
